d7ad3e049a04bfe2edbed145c339a6ae439c6047
furzoom <mn@furzoom.com>
2016-01-27 18:02:27 +0800

    add GetExitCodeThread

--- win32/thread/thread.cpp
+++ win32/thread/thread.cpp
@@ -7,10 +7,23 @@ int main(int argc, char* argv[])
 {
     HANDLE hThread;
     DWORD dwThreadId;
+    DWORD dwExitCode;
 
     hThread = ::CreateThread(NULL, NULL, ThreadProc, NULL, 0, &dwThreadId);
     printf("Now another thread has been created. ID = %d\n", dwThreadId);
 
+    while (::GetExitCodeThread(hThread, &dwExitCode))
+    {
+        if (dwExitCode == STILL_ACTIVE)
+        {
+            printf("STILL_ACTIVE\n");
+        }
+        else
+        {
+            printf("Exit Code: %d\n", dwExitCode);
+            break;
+        }
+    }
     // wait for thread terminate
     ::WaitForSingleObject(hThread, INFINITE);
     ::CloseHandle(hThread);

3a3d298c4c74bb3c101d8abbb6c6ee603307219e
furzoom <mn@furzoom.com>
2016-01-28 09:40:44 +0800

    WaitForMultipleObjects

--- win32/threadPriority/threadPriority.cpp
+++ win32/threadPriority/threadPriority.cpp
@@ -13,11 +13,32 @@ int main(int argc, char* argv[])
     ::SetThreadPriority(h[0], THREAD_PRIORITY_IDLE);
     ::ResumeThread(h[0]);
 
-    h[1] = ::CreateThread(NULL, 0, threadNormal, NULL, CREATE_SUSPENDED, &dwThreadId);
-    ::SetThreadPriority(h[1], THREAD_PRIORITY_NORMAL);
-    ::ResumeThread(h[1]);
+    h[1] = ::CreateThread(NULL, 0, threadNormal, NULL, 0, &dwThreadId);
+//    ::SetThreadPriority(h[1], THREAD_PRIORITY_NORMAL);
+//    ::ResumeThread(h[1]);
 
-    ::WaitForMultipleObjects(sizeof(h) / sizeof(HANDLE), h, TRUE, INFINITE);
+    DWORD dwRet;
+    int iCount = 0;
+    while (iCount != 2)
+    {
+        dwRet = ::WaitForMultipleObjects(2, h, FALSE, 1);
+        switch (dwRet)
+        {
+        case WAIT_FAILED:
+            printf("WaitForMultipleObjects failed!\n");
+            break;
+        case WAIT_TIMEOUT:
+            printf("Time out!\n");
+            iCount = 2;
+            break;
+        case WAIT_OBJECT_0 + 0:
+            printf("Idle signaled\n");
+            break;
+        case WAIT_OBJECT_0 + 1:
+            printf("Normal signaled\n");
+            break;
+        }
+    }
 
     ::CloseHandle(h[0]);
     ::CloseHandle(h[1]);
